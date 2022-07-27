c008a5abaad0f92e715b6ee5abdcb6c4b97b558d
kwidj001 <kwidj001@localhost>
2022-07-27 11:15:06 -0600

    check plot

--- logger.py
+++ logger.py
@@ -2,20 +2,23 @@
 import matplotlib.pyplot as plt
 
 time = [i for i in range(30)]
-cpu_usage = []
+cpu_usage = [0 for i in range(30)]
 
 # Get %CPU from FLAME controller log
 log_file = open("logs/controller.cpu")
 
 lines = log_file.readlines()[3:-1]
 
+count = 0
 for line in lines:
     print(float(line.split()[8]))
-    cpu_usage.append(line.split()[8])
+    cpu_usage[count] += float(line.split()[8])
+    count += 1
 
 log_file.close()
 
-cpu_usage = list(map(float, cpu_usage))
+#cpu_usage = list(map(float, cpu_usage))
+
 # plot time (seconds) vs %CPU logs
 plt.plot(time, cpu_usage)
 plt.title('Time vs flame-controller cpu usage: during hier_mnist example')
