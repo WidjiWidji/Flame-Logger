7b4dfdaf64394669d2131fbd1d4321ee14829a9d
kwidj001 <kwidj001@localhost>
2022-07-27 15:27:19 -0600

    run pidstat for 1hr15

--- med_mnist/get_log.sh
+++ med_mnist/get_log.sh
@@ -8,11 +8,9 @@ JOB_ID="62e1661d6a9a1d6fd8d9f6c3"
 FLAME_TASKS=$(flamectl get tasks $JOB_ID --insecure | awk '{print $4}' | grep -oE '[0-9a-z]+' | tail -n +2)
 
 aggregator_task_id=""
+
 for task in ${FLAME_TASKS[@]}; do
 	role=$(flamectl get task $JOB_ID $task --insecure | awk '{print $2}' | sed -n 5p | grep -oE '[0-9a-z]+')
-	
-	echo "TaskID: $task | role: $role"
-	
 	if [ "$role" == "aggregator" ]; then
 		aggregator_task_id=$task
 		break
@@ -24,11 +22,11 @@ flamectl get jobs --insecure
 
 flamectl start job $JOB_ID --insecure
 
-# Get ContainerID and ProcessID 
+# Get ContainerID and ProcessID for aggregator flamelet agent
 CONTAINER_ID=$(kubectl get pods -l job-name="flame-agent-${aggregator_task_id}" -n flame -o json | jq -r '.items[].status.containerStatuses[] |.containerID' | awk '{print substr($0, 14)}')
 
 PROCESS_ID=$(crictl inspect --output go-template --template '{{.info.pid}}' $CONTAINER_ID)
 
 # Get process resource usage
-pidstat 1 20 -p $PROCESS_ID > logs/agent.cpu
+pidstat 1 4500 -p $PROCESS_ID > logs/agent_cpu.cpu
 
