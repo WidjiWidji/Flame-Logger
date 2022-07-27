57469c522732561285f990bcdec1cacb99454145
kwidj001 <kwidj001@localhost>
2022-07-27 11:59:58 -0600

    move to folder

--- hier_mnist/logger.py
+++ hier_mnist/logger.py
@@ -1,6 +1,7 @@
 # READ CONTROLLER.CPU and create graphs from data
 import matplotlib.pyplot as plt
 
+# hold time and cpu usage for 30 seconds for plotting
 time = [i for i in range(30)]
 cpu_usage = [0 for i in range(30)]
 
@@ -17,11 +18,10 @@ for line in lines:
 
 log_file.close()
 
-#cpu_usage = list(map(float, cpu_usage))
 
 # plot time (seconds) vs %CPU logs
 plt.plot(time, cpu_usage)
-plt.title('Time vs flame-controller cpu usage: during hier_mnist example')
+plt.title('Time vs flame-controller %CPU:hier_mnist example')
 plt.xlabel('Time (seconds)')
 plt.ylabel('%CPU')
 plt.savefig('plots/controller_test1.png')
